cab03b227f91e24e6c1f9a2bac2ea82a6926c1de
Liu Yang <zation1@gmail.com>
2022-08-22 13:50:27 +0800

    hot reload express server

--- index.ts
+++ index.ts
@@ -1,64 +1,70 @@
 import { globby } from 'globby';
-import express, { Express } from 'express';
-import chokidar from 'chokidar';
+import express, {
+  Express,
+  Handler,
+} from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-
 import type {
   Plugin,
-  AliasOptions,
-} from 'vite'
-import moduleAlias from 'module-alias';
+  ViteDevServer,
+} from 'vite';
+import { resolve } from 'path';
 
 export interface Options {
   middlewareFiles: string | string[]
-  alias?: Record<string, string>
-  expressOptions
+  defaultMiddlewares?: Handler[]
 }
 
-const startApp = async (app: Express, middlewareFiles: string | string[]) => {
-  app.use(cors());
-  app.use(bodyParser.json());
-  app.use((req, res, next) => {
-    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-    res.header('Expires', '-1');
-    res.header('Pragma', 'no-cache');
-    next();
-  });
+interface Middlewares {
+  [path: string]: Handler
+}
+
+const startApp = async (app: Express, middlewareFiles: string | string[], server: ViteDevServer, defaultMiddlewares?: Handler[]) => {
+  let middlewares: Middlewares = {};
+  if (defaultMiddlewares) {
+    defaultMiddlewares.forEach((middleware) => {
+      app.use(middleware);
+    });
+  } else {
+    app.use(cors());
+    app.use(bodyParser.json());
+    app.use((req, res, next) => {
+      res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+      res.header('Expires', '-1');
+      res.header('Pragma', 'no-cache');
+      next();
+    });
+  }
 
   const paths = await globby(middlewareFiles);
-  paths.forEach((path) => {
-    app.use(require(path).default);
+  await Promise.all(paths.map(async (path) => {
+    const fullPath = resolve(process.cwd(), path);
+    middlewares[fullPath] = (await server.ssrLoadModule(fullPath)).default;
+    server.watcher.add(resolve(process.cwd(), path));
+  }));
+  app.use((req, res, next) => {
+    Object.values(middlewares).forEach((middleware) => middleware(req, res, next));
+  });
+  server.watcher.on('change', async (path) => {
+    if (middlewares[path]) {
+      middlewares[path] = (await server.ssrLoadModule(resolve(process.cwd(), path))).default;
+    }
   });
 
   return app;
 };
 
-const addAlias = (alias: Record<string, string> | AliasOptions) => {
-  Object.keys(alias).forEach((key) => {
-    moduleAlias.addAlias(key, alias[key]);
-  });
-}
-
-export default ({ middlewareFiles, alias }: Options): Plugin => {
+export default ({ middlewareFiles, defaultMiddlewares }: Options): Plugin => {
   const app = express();
-  if (alias) {
-    addAlias(alias);
-  }
   return {
     name: 'vite:middleware',
-    config: async (config) => {
-      const { resolve: { alias: viteAlias } } = config;
-      if (viteAlias && !alias) {
-        addAlias(viteAlias);
-      }
-      await startApp(app, middlewareFiles);
-      chokidar.watch(middlewareFiles).on('change', (path) => {
-        app.use(require(path).default);
-      });
-    },
+    apply: 'serve',
     configureServer: (server) => {
       server.middlewares.use(app);
+      return async () => {
+        await startApp(app, middlewareFiles, server, defaultMiddlewares);
+      }
     },
   };
 }
